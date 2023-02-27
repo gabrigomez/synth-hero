fb0a69d41548eedf16f729f6b6243121e4d8ab96
Gabriel <gabrigomez15@gmail.com>
2023-02-27 12:36:44 -0300

    feat: add keys

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,8 +3,6 @@ import { GiPianoKeys } from "react-icons/gi";
 import { FaDrumSteelpan } from "react-icons/fa";
 import { FaGuitar } from "react-icons/fa";
 
-
-
 export const Main = () => {
   return (
     <div className='flex flex-col h-2/4 justify-around items-center'>
@@ -34,8 +32,28 @@ export const Main = () => {
               <div className='border-2 border-black bg-zinc-600 rounded-full h-16 w-16'></div>
             </div>
           </div>
-          <div className='h-2/4'>
-            keys
+          <div className='flex justify-center w-full h-2/4'>
+            <div className='flex items-center justify-center bg-black rounded-md w-full h-full'>
+              <ul className='flex relative bg-white w-10/12 h-5/6'>
+                <li className='flex flex-col justify-end w-[10%] h-full text-xs text-slate-400'>A</li>                
+                <li className='h-2/4 absolute left-[24px] w-4 bg-black z-20'>W</li>
+                <li className='flex flex-col justify-end border border-gray-100 w-[10%] h-full text-xs text-slate-400'>S</li>
+                <li className='h-2/4 absolute left-[56px] w-4 bg-black z-20'>E</li>                
+                <li className='flex flex-col justify-end border border-gray-100 w-[10%] h-full text-xs text-slate-400'>D</li>
+                <li className='flex flex-col justify-end border border-gray-100 w-[10%] h-full text-xs text-slate-400'>F</li>
+                <li className='h-2/4 absolute left-[120px] w-4 bg-black z-20'>E</li>
+                <li className='flex flex-col justify-end border border-gray-100 w-[10%] h-full text-xs text-slate-400'>G</li>
+                <li className='h-2/4 absolute left-[152px] w-4 bg-black z-20'>Y</li>
+                <li className='flex flex-col justify-end border border-gray-100 w-[10%] h-full text-xs text-slate-400'>H</li>
+                <li className='h-2/4 absolute left-[184px] w-4 bg-black z-20'>U</li>
+                <li className='flex flex-col justify-end border border-gray-100 w-[10%] h-full text-xs text-slate-400'>J</li>
+                <li className='flex flex-col justify-end border border-gray-100 w-[10%] h-full text-xs text-slate-400'>K</li>
+                <li className='h-2/4 absolute left-[248px] w-4 bg-black z-20'>I</li>
+                <li className='flex flex-col justify-end border border-gray-100 w-[10%] h-full text-xs text-slate-400'>L</li>
+                <li className='h-2/4 absolute left-[280px] w-4 bg-black z-20'>O</li>
+                <li className='flex flex-col justify-end border border-gray-100 w-[10%] h-full text-xs text-slate-400'>Ç</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
