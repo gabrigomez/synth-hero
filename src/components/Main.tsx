c326226196a01b5013cccf52c4d9ba3fb6671f91
Gabriel <gabrigomez15@gmail.com>
2023-02-27 10:13:02 -0300

    feat: overwrite sm breakpoint and start synth layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,9 +2,30 @@ import * as React from 'react';
 
 export const Main = () => {
   return (
-    <div>
+    <div className='flex flex-col h-2/4 justify-around items-center'>
       <div>
         Synth Hero
+      </div>
+      <div className='bg-zinc-700 h-56 w-11/12 sm:w-96 rounded-xl'>
+        <div className='h-6 bg-zinc-900 rounded-t-xl'></div>
+        <div className='flex flex-col h-[200px]'>
+          <div className='flex h-2/4 w-full'>
+            <div className='w-1/4'>
+              L
+            </div>
+            <div className='w-2/4'>
+              Presets Led
+            </div>
+            <div className='w-1/4'>
+              R
+            </div>
+          </div>
+          <div className='h-2/4'>
+            keys
+          </div>
+
+        </div>
+
       </div>
     </div>
   )
