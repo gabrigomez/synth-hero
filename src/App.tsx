ebb8903d28437288836ab3b8c3238d8e5c0cf913
Gabriel <gabrigomez15@gmail.com>
2023-02-27 09:45:21 -0300

    feat: add tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Main } from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        Init
-      </div>
+    <div className="App bg-gradient-to-b from-cyan-100 to-slate-100 h-screen">
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const Main = () => {
+  return (
+    <div>
+      <div>
+        Synth Hero
+      </div>
+    </div>
+  )
+}
