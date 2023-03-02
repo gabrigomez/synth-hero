0dc15e0891a560bd0d2b2cb6fe90d666b7943e83
Gabriel <gabrigomez15@gmail.com>
2023-03-02 13:16:19 -0300

    feat: add same method to click and keyboard events on synth

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { GiPianoKeys } from "react-icons/gi";
 import { FaDrumSteelpan } from "react-icons/fa";
 import { FaGuitar } from "react-icons/fa";
@@ -7,16 +7,16 @@ import { noteMap, playNote } from '../utils/Notes';
 
 export const Main = () => {
   useEffect(() => {
-    document.addEventListener('keydown', handleNote, true);
+    document.addEventListener('keydown', playNoteHandler, true);
   });
 
-  const handleNote = (event: any) => {
-    playNote(noteMap[event.key as keyof typeof noteMap])  //play the note according with the noteMap object
-  };
-
-  const playNoteOnClick = (event: any) => {
-    const note = event.toLowerCase()
-    playNote(noteMap[note as keyof typeof noteMap])
+  const playNoteHandler = (event: any) => {
+    if(event.key) {
+      playNote(noteMap[event.key as keyof typeof noteMap]); //play the note according with the noteMap object
+    } else {
+      const note = event.toLowerCase();
+      playNote(noteMap[note as keyof typeof noteMap]);
+    };
   };
 
   return (
@@ -54,55 +54,55 @@ export const Main = () => {
           <div className='flex justify-center w-full h-2/4'>
             <div className='flex items-center justify-center bg-black rounded-md w-full h-full'>
               <ul className='flex relative bg-white w-10/12 h-5/6'>
-                <li className='key white-keys w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>                  
+                <li className='key white-keys w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>                  
                   A                  
                 </li>                
-                <li className='black-keys left-[18px] sm:left-[24px]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='black-keys left-[18px] sm:left-[24px]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   W                  
                 </li>
-                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   S                  
                 </li>
-                <li className='black-keys left-[42px] sm:left-[56px]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='black-keys left-[42px] sm:left-[56px]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   E                  
                 </li>                
-                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   D
                 </li>
-                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   F
                 </li>
-                <li className='black-keys left-[90px] sm:left-[120px]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='black-keys left-[90px] sm:left-[120px]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   T
                 </li>
-                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   G
                 </li>
-                <li className='black-keys left-[114px] sm:left-[152px]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='black-keys left-[114px] sm:left-[152px]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   Y
                 </li>
-                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   H
                 </li>
-                <li className='black-keys left-[138px] sm:left-[184px]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='black-keys left-[138px] sm:left-[184px]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   U
                 </li>
-                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   J
                 </li>
-                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   K
                 </li>
-                <li className='black-keys left-[186px] sm:left-[248px]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='black-keys left-[186px] sm:left-[248px]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   O
                 </li>
-                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   L
                 </li>
-                <li className='black-keys left-[210px] sm:left-[280px]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='black-keys left-[210px] sm:left-[280px]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   P
                 </li>
-                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteOnClick(e.target.innerText)}>
+                <li className='key white-keys border border-gray-100 w-[10%]' onClick={(e:any)=> playNoteHandler(e.target.innerText)}>
                   Ç
                 </li>
               </ul>
